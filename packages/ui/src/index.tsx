b1a2bbeca72d8bfa4ee04aa40a55bad69535948c
JayashTripathy <jayashtripathy371@gmail.com>
2024-09-01 18:09:23 +0530

    :sparkles: added rounding options in the docs ui

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,5 +3,5 @@ import "./globals.css"
 export * from "./components/button"
 export * from "./components/input"
 export * from "./components/label"
-
+export * from "./components/select"
 export * from "./lib/utils"
