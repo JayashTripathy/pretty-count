c2e9b89e5b3bd4000de765de52d70e7b539d952b
JayashTripathy <jayashtripathy371@gmail.com>
2024-09-14 23:00:57 +0530

    updated footer

--- apps/web/app/components/footer.tsx
+++ apps/web/app/components/footer.tsx
@@ -7,7 +7,7 @@ function Footer({}: Props) {
     <footer className="bg-secondary/30 px-10 py-2   mt-20  text-center">
     <div className="text-xs text-secondary-foreground/50">
       Created by{" "}
-      <a href="https://github.com/suryakant-kumar" target="_blank">
+      <a href="https://github.com/JayashTripathy" target="_blank">
        Jayash Tripathy
       </a>
     </div>
